1120e6cd91db0b686230ac5fd7afec0e668a606f
LoubatTh <thomas.loubat@epitech.eu>
2024-06-18 15:13:39 +0200

    build(back): add Makefile

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -8,4 +8,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
+// Clear tokens that are a week old (to avoid deleting refresh-tokens)
 Schedule::command('sanctum:prune-expired --hours=168')->daily();

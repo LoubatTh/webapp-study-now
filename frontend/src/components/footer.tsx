bb553e5002ce3350c9a2ad1d26cf9b5e9c855fbb
Steven Dorion <112935133+Noiirod@users.noreply.github.com>
2024-06-24 16:49:45 +0200

    fix: improve footer for responsive

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,28 +1,53 @@
 const Footer = () => {
   return (
-    <div className=" h-200 py-4 px-10 text-sm shadow-md">
-      <div className="flex content-center justify-between">
-        <div className="">
-          <div className="p-2">
-            <img></img>
-            <p>Logo du site</p>
+    <div className="">
+      <div className="hidden sm:block h-200 py-4 px-10 text-sm shadow-md">
+        <div className="flex content-center justify-between">
+          <div className="">
+            <div className="p-2">
+              <img></img>
+              <p>Logo du site</p>
+            </div>
+            <div className="px-1 py-4">
+              <p>Rendre l'apprentissage plus simple</p>
+            </div>
           </div>
-          <div className="px-1 py-4">
-            <p>Rendre l'apprentissage plus simple</p>
+          <div className="flex">
+            <div className="p-2 content-center">
+              <ul>
+                <a className="p-1 underline" href="">github</a>
+                <a className="p-1 underline" href="">contact</a>
+              </ul>
+            </div>
           </div>
         </div>
-        <div className="flex">
-          <div className="p-2 content-center">
-            <ul>
-              <a className="p-1" href="">github</a>
-              <a className="p-1" href="">contact</a>
-            </ul>
-          </div>
+        <hr className="border-t border-gray-300"></hr>
+        <div className="px-1 py-4">
+          <p className="text-xs">© 2020 'nom du site', Inc. Tout droit réservé.</p>
         </div>
       </div>
-      <hr className="border-t border-gray-300"></hr>
-      <div className="px-1 py-4">
-        <p className="text-xs">© 2020 'nom du site', Inc. Tout droit réservé.</p>
+      <div className="sm:hidden h-200 py-4 px-10 text-sm shadow-md">
+        <div className="flex content-center justify-between">
+          <div className="">
+            <div className="p-2">
+              <img></img>
+              <p>Logo du site</p>
+            </div>
+            <div className="p-2">
+              <p>Rendre l'apprentissage plus simple</p>
+            </div>
+            <div className="px-1 pt-1 pb-4">
+              <ul>
+                <a className="p-1 underline" href="">github</a>
+                <a className="p-1 underline" href="">contact</a>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <hr className="border-t border-gray-300"></hr>
+        <div className="px-1 py-4">
+          <p className="text-xs">© 2020 'nom du site', Inc. Tout droit réservé.</p>
+        </div>
       </div>
     </div>
   );

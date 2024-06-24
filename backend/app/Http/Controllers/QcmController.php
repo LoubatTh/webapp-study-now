75a111777dad40fd54290a20161b0f4c2674c26f
Scott Fitzgerald <john.fitzgerald@epitech.eu>
2024-06-24 11:36:27 +0200

    feat(controller): add 'store' method to 'post' a qcm

--- backend/app/Http/Controllers/QcmController.php
+++ backend/app/Http/Controllers/QcmController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Qcm;
+use Illuminate\Http\JsonResponse;
 
 class QcmController extends Controller
 {
     //
+
+
+    public function store(Request $request): JsonResponse
+    {
+
+        $qcm = Qcm::create([
+            'question' => $request->question,
+            'answers' => $request->answers
+        ]);
+
+        return response()->json($qcm, 201);
+    }
+
 }

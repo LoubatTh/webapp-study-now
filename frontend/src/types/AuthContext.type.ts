e6f0389d14c473f2f4d199f3ab2a63174afd99bb
Nathan Dulac <nathan.dulac@epitech.eu>
2024-06-24 17:02:55 +0200

    fix(AuthContext): fix grammatical error

--- frontend/src/types/AuthContext.type.ts
+++ frontend/src/types/AuthContext.type.ts
@@ -1,5 +1,5 @@
 export type AuthContextType = {
-    accessToken: string | null; //A voir si laisser ce champ accessible partout dans le context est utile (car un peu dangereux je trouve)
+    accessToken: string | null; //A voir si laisser ce champ accessible partout dans le context est util (car un peu dangereux je trouve)
     expiresAt: number | null;
     isReady: boolean;
     setToken: (accessToken: string, accessTokenExpiration: string, refreshToken: string, refreshTokenExpiration: string) => void;

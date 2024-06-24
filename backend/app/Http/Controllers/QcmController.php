2c9bea07a12b3827a6fd8a8c543682b8a4f2b6ab
Scott Fitzgerald <john.fitzgerald@epitech.eu>
2024-06-24 15:15:35 +0200

    feat(controller): import 'Controller' class

--- backend/app/Http/Controllers/QcmController.php
+++ backend/app/Http/Controllers/QcmController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Qcm;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Routing\Controller;
 
-class QcmController
+class QcmController extends Controller
 {
     //
 

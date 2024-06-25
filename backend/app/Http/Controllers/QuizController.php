31e43a23e04d5f392de2a98b0cc3dbe4224c79b2
Scott Fitzgerald <john.fitzgerald@epitech.eu>
2024-06-25 13:46:00 +0200

    feat(controllers): add 'store' method to post a quiz

--- backend/app/Http/Controllers/QuizController.php
+++ backend/app/Http/Controllers/QuizController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 
 class QuizController extends Controller
 {
     //
+
+
+    public function store(Request $request): JsonResponse
+    {
+        $qcm = Qcm::create([
+            'name' => $request->name,
+        ]);
+
+    }
 }

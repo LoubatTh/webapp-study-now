4d8fcc00dc8ef569f1a4769f83b24f5685ddd0c5
Matteo-Degano <mdegano@webdev.com>
2024-07-01 09:52:59 +0200

    feat: added creation for quizz feat: added responsive view feat: added tests for quizz

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,26 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from "./contexts/AuthContext";
 import RouteChangeListener from "./listeners/routes/RouteChangeListener";
 import LoginPage from "./pages/LoginPage";
+import CreateQuizzPage from "./pages/CreateQuizzPage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-      <BrowserRouter>
+    <BrowserRouter>
       <AuthProvider>
         <RouteChangeListener />
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/create-quizz" element={<CreateQuizzPage />} />
         </Routes>
-        </AuthProvider>
-      </BrowserRouter>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );

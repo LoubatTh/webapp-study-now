20ea7a37698dd0a3a49f6119c431c7194519df23
Matteo-Degano <mdegano@webdev.com>
2024-07-01 09:54:16 +0200

    feat: added missing files

--- frontend/src/listeners/routes/RouteChangeListener.tsx
+++ frontend/src/listeners/routes/RouteChangeListener.tsx
@@ -1,51 +1,42 @@
-import { useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useAuth } from '../../contexts/AuthContext';
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 
 /*
 Mettre ici toutes les routes que l'on souhaite exclure de la vérification d'authentification
 */
-const excludedRoutes = [
-  '/',
-  '/login',
-];
+const excludedRoutes = ["/", "/login", "/create-quizz"];
 
 const RouteChangeListener = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { checkToken, logout } = useAuth();
 
-   const location = useLocation();
-   const navigate = useNavigate();
-   const { checkToken, logout } = useAuth();
-
-    useEffect(() => {
-
-      const verifyAccess = async () => {
-
-        /*
+  useEffect(() => {
+    const verifyAccess = async () => {
+      /*
           Si cette page ne nécessite pas d'authentification alors on ne fait rien
           */
-        if (excludedRoutes.includes(location.pathname)) {
-          return;
-        }
+      if (excludedRoutes.includes(location.pathname)) {
+        return;
+      }
 
-        
-        const tokenIsValid = await checkToken();
+      const tokenIsValid = await checkToken();
 
-        /*
+      /*
           Si le token n'est plus valide alors on redirige vers la page de login
           */
-        if (!tokenIsValid) {
-          console.log("La validation du token a échoué, redirection vers /login");
-          logout();
-          navigate("/login");
-        }
-      };
+      if (!tokenIsValid) {
+        console.log("La validation du token a échoué, redirection vers /login");
+        logout();
+        navigate("/login");
+      }
+    };
 
     verifyAccess();
+  }, [location]);
 
-    }, [location]);
-
-     return null;
-
+  return null;
 };
 
 export default RouteChangeListener;

3861b6c88213bfed2415835666563dd7133ce1cf
Scott Fitzgerald <john.fitzgerald@epitech.eu>
2024-07-01 17:22:33 +0200

    test(quiz): add test_creates_quiz

--- backend/tests/Feature/QuizTest.php
+++ backend/tests/Feature/QuizTest.php
@@ -4,17 +4,72 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
+use Illuminate\Foundation\Testing\TestCase;
+use App\Models\User;
+use App\Models\Quiz;
+
 
 class QuizTest extends TestCase
-{
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+{   
 
-        $response->assertStatus(200);
+    public function test_creates_quiz(): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $quizData = [
+            "name" => 'quiz 1',
+            'isPublic' => true,
+            'isOrganization' => false,
+            'qcms' => [
+                [
+                    'question' => 'question 1',
+                    'answers' => [
+                        [
+                            "response" => "A",
+                            "isValid" => true
+                        ],
+                        [
+                            "response" => "B",
+                            "isValid" => true
+                        ],
+                        [
+                            "response" => "C",
+                            "isValid" => false
+                        ],
+                        [
+                            "response" => "D",
+                            "isValid" => false
+                        ]
+                    ]
+                ],
+                [
+                    'question' => 'question 2',
+                    'answers' => [
+                        [
+                            "response" => "1",
+                            "isValid" => true
+                        ],
+                        [
+                            "response" => "2",
+                            "isValid" => true
+                        ],
+                        [
+                            "response" => "3",
+                            "isValid" => false
+                        ],
+                        [
+                            "response" => "4",
+                            "isValid" => false
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $response = $this->post('/quizzes', $quizData);
+
+        $response->assertStatus(201);
+        $this->assertDatabaseHas('quizzes', ['name' => 'quiz 1']);
     }
 }

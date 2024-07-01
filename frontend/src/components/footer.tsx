24907a2c3282172f71e1670cfa6c8a34b4e73af8
Steven Dorion <112935133+Noiirod@users.noreply.github.com>
2024-07-01 17:20:03 +0200

    fix: change the footer

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -24,7 +24,11 @@ const Footer = () => {
               </div>
             </div>
             <div className="px-1 py-4">
-              <p>Rendre l'apprentissage plus simple</p>
+              {/* <p>Rendre l'apprentissage plus simple</p> */}
+              <p>
+                Elle est dispo, moi aussi, j'crois qu'on va réviser grâce à
+                StudyNow
+              </p>
             </div>
           </div>
           <div className="flex">
@@ -42,20 +46,32 @@ const Footer = () => {
         </div>
         <hr className="border-t border-gray-300"></hr>
         <div className="px-1 py-4">
-          <p className="text-xs">
-            © 2020 'nom du site', Inc. Tout droit réservé.
-          </p>
+          <p className="text-xs">© 2024 StudyNow, Inc. Tout droit réservé.</p>
         </div>
       </div>
       <div className="sm:hidden h-200 py-4 px-10 text-sm shadow-md">
         <div className="flex content-center justify-between">
           <div className="">
-            <div className="p-2">
-              <img></img>
-              <p>Logo du site</p>
+            <div className="flex items-center">
+              <div>
+                <ImageLink
+                  href="/"
+                  src={logo}
+                  alt="logo du site"
+                  width="w-12"
+                  height="h-12"
+                />
+              </div>
+              <div>
+                <h3>StudyNow</h3>
+              </div>
             </div>
             <div className="p-2">
-              <p>Rendre l'apprentissage plus simple</p>
+              {/* <p>Rendre l'apprentissage plus simple</p> */}
+              <p>
+                Elle est dispo, moi aussi, j'crois qu'on va réviser grâce à
+                StudyNow
+              </p>
             </div>
             <div className="px-1 pt-1 pb-4">
               <ul>
@@ -71,9 +87,7 @@ const Footer = () => {
         </div>
         <hr className="border-t border-gray-300"></hr>
         <div className="px-1 py-4">
-          <p className="text-xs">
-            © 2020 'nom du site', Inc. Tout droit réservé.
-          </p>
+          <p className="text-xs">© 2024 StudyNow, Inc. Tout droit réservé.</p>
         </div>
       </div>
     </div>

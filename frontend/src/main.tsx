2f6b275e15727c19c913772d07431c0a7d5bb660
Matteo-Degano <mdegano@webdev.com>
2024-07-01 09:52:59 +0200

    feat: added creation for quizz feat: added responsive view feat: added tests for quizz

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,34 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from "./contexts/AuthContext";
 import RouteChangeListener from "./listeners/routes/RouteChangeListener";
 import { UserProvider } from "./contexts/UserContext";
 import ProfilePage from "./pages/ProfilePage";
 import LegacyLoginPage from "./pages/LegacyLoginPage";
 import LoginPage from "./pages/LoginPage";
+import CreateQuizzPage from "./pages/CreateQuizzPage";
 import { Toaster } from "./components/ui/toaster";
 
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <BrowserRouter>
-    <Toaster/>
-      <AuthProvider>
-        <UserProvider>
-          <RouteChangeListener />
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/legacylogin" element={<LegacyLoginPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/login" element={<LoginPage />} />
-          </Routes>
-        </UserProvider>
-      </AuthProvider>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Toaster />
+    <AuthProvider>
+      <UserProvider>
+        <RouteChangeListener />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/legacylogin" element={<LegacyLoginPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/create-quizz" element={<CreateQuizzPage />} />
+        </Routes>
+      </UserProvider>
+    </AuthProvider>
+  </BrowserRouter>
 );

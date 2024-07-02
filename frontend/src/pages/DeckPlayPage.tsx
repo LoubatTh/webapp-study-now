2c89c0b90eacc7d42d026411228f55d458ca3fd1
Matteo-Degano <mdegano@webdev.com>
2024-07-02 10:12:06 +0200

    feat: base page Deck play

--- frontend/src/pages/DeckPlayPage.tsx
+++ frontend/src/pages/DeckPlayPage.tsx
@@ -1,7 +1,6 @@
 import DeckComponent from "@/components/deck/DeckComponent";
-import { Deck } from "@/types/deck.type";
 import { fetchApi } from "@/utils/api";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const getDeck = async (deckId: string | undefined) => {
@@ -11,25 +10,26 @@ const getDeck = async (deckId: string | undefined) => {
 
 const DeckPlayPage = () => {
   const { deckId } = useParams();
-  const [deck, setDeck] = useState<Deck | undefined>(undefined);
+  const [deck, setDeck] = useState({});
 
   const fetchDeck = async () => {
     const response = await getDeck(deckId);
     if (response.status === 200) {
-      if (response.data !== null) {
-        const data: Deck = response.data;
-        console;
-        setDeck(data);
-      }
+      const data = response.data;
+      setDeck(data);
+      console.log(data);
     }
   };
 
   useEffect(() => {
     fetchDeck();
-    console.log(deck);
-  }, []);
+  });
 
-  return <div>{/* <DeckComponent /> */}</div>;
+  return (
+    <div>
+      <DeckComponent flashcards={} />
+    </div>
+  );
 };
 
 export default DeckPlayPage;

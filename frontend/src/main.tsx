75af576b116443e2a2b9761038529eb5a6a9b693
Steven Dorion <112935133+Noiirod@users.noreply.github.com>
2024-07-02 11:23:24 +0200

    Revert "Feat/board"

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,34 +2,29 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import LoginPage from "./pages/LoginPage";
-import CreateQuizzPage from "./pages/CreateQuizzPage";
-import CreateDeckPage from "./pages/CreateDeckPage";
-import { Toaster } from "./components/ui/toaster";
-import BoardPage from "./pages/BoardPage";
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from "./contexts/AuthContext";
 import RouteChangeListener from "./listeners/routes/RouteChangeListener";
 import { UserProvider } from "./contexts/UserContext";
 import ProfilePage from "./pages/ProfilePage";
-import LayoutNavbarPage from "./pages/LayoutNavbarPage";
+import LoginPage from "./pages/LoginPage";
+import CreateQuizzPage from "./pages/CreateQuizzPage";
+import { Toaster } from "./components/ui/toaster";
+import CreateDeckPage from "./pages/CreateDeckPage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-      <BrowserRouter>
-        <Toaster />
-        <AuthProvider>
-          <UserProvider>
-            <RouteChangeListener />
-            <Routes>
-              <Route path="/" element={<LayoutNavbarPage />}>
-                <Route path="" element={<Homepage />} />
-                <Route path="/board" element={<BoardPage />} />
-                <Route path="/create-quizz" element={<CreateQuizzPage />} />
-                <Route path="/create-deck" element={<CreateDeckPage />} />
-              </Route>
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/profile" element={<ProfilePage />} />
-            </Routes>
-          </UserProvider>
-        </AuthProvider>
-      </BrowserRouter>
+  <BrowserRouter>
+    <Toaster />
+    <AuthProvider>
+      <UserProvider>
+        <RouteChangeListener />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/create-quizz" element={<CreateQuizzPage />} />
+          <Route path="/create-deck" element={<CreateDeckPage />} />
+        </Routes>
+      </UserProvider>
+    </AuthProvider>
+  </BrowserRouter>
 );

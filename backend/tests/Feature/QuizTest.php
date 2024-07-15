a9a032faa789bfdf507a099671e3ae2af657cf51
Scott Fitzgerald <john.fitzgerald@epitech.eu>
2024-07-15 12:00:21 +0200

    test(quiz): add setUp method

--- backend/tests/Feature/QuizTest.php
+++ backend/tests/Feature/QuizTest.php
@@ -13,22 +13,29 @@ use App\Models\Tag;
 class QuizTest extends TestCase
 {   
 
+    private $user;
+    private $quizData;
+    private $tag;
+    // SET_UP
 
-    // User is authenticated
-
-    public function test_creates_quiz(): void
+    protected function setUp(): void
     {
-        $user = User::factory()->create();
-        $this->actingAs($user);
 
-        $tag = Tag::factory()->create();
+        parent::setUp();
 
-        $quizData = [
+        // INIT USER
+        $this->user = User::factory()->create();
+        $this->actingAs($this->user);
+
+        $this->tag = Tag::factory()->create();
+
+        // INIT PUBLIC QUIZ
+        $this->quizData = [
             "name" => 'quiz 1',
             'is_public' => true,
             'is_organization' => false,
-            'tag_id' => $tag->id,
-            'user_id' => $user->id,
+            'tag_id' => $this->tag->id,
+            'user_id' => $this->user->id,
             'qcms' => [
                 [
                     'question' => 'question 1',
@@ -74,6 +81,17 @@ class QuizTest extends TestCase
                 ]
             ]
         ];
+    }
+
+
+    // User is authenticated
+
+    public function test_creates_quiz(): void
+    {
+        // $user = User::factory()->create();
+        // $this->actingAs($user);
+
+        // $tag = Tag::factory()->create();
 
         $quizDetails = [
             "name" => 'quiz 1',
@@ -81,14 +99,14 @@ class QuizTest extends TestCase
             'is_organization' => false
         ];
 
-        $response = $this->post('/api/quizzes', $quizData);
+        $response = $this->post('/api/quizzes', $this->quizData);
 
         $response->assertStatus(201);
         $this->assertDatabaseHas('quizzes', ['name' => 'quiz 1']);
 
 
         $quizId = $response->json('id');
-        $qcms = $quizData["qcms"];
+        $qcms = $this->quizData["qcms"];
 
         foreach ($qcms as $qcm) {
 
@@ -97,54 +115,6 @@ class QuizTest extends TestCase
             $this->assertEquals($qcm['question'], $dbQcm->question);
             $this->assertEquals($qcm['answers'], $dbQcm->answers);
         }
-    }
-
-    public function test_updates_quiz(): void
-    {
-
-        $user = User::factory()->create();
-        $this->actingAs($user);
-
-        $quiztest = Quiz::factory()->hasQcms(2)->create([
-            "name" => 'Quiz Test',
-            'is_public' => true,
-            'is_organization' => false,
-            'user_id' => $user->id
-        ]);
-
-
-        $quizData = [
-            'name' => 'Quiz updated',
-            'is_public' => false,
-            'is_organization' => true,
-            'qcms' => [
-                [
-                    'question' => 'Question updated',
-                    'answers' => [
-                        [
-                            'answer' => 'Answer 1 updated',
-                            'isValid' => true
-                        ],
-                        [
-                            'answer' => 'Answer 2 updated',
-                            'isValid' => false
-                        ],
-                        [
-                            'answer' => 'Answer 3 updated',
-                            'isValid' => false
-                        ],
-                        [
-                            'answer' => 'Answer 4 updated',
-                            'isValid' => true
-                        ]
-                    ]
-                ]
-            ]
-        ];
-
-        $response = $this->put('/api/quizzes/' . $quiztest->id, $quizData);
-        // $response->assertStatus(200);
-        $this->assertDatabaseHas('quizzes', ['name' => 'Quiz updated']);
     }
 
     public function test_deletes_quiz(): void
@@ -183,4 +153,55 @@ class QuizTest extends TestCase
         
         $this->assertNotNull($response->json());
     }
+
+
+
+
+    // public function test_updates_quiz(): void
+    // {
+
+    //     $user = User::factory()->create();
+    //     $this->actingAs($user);
+
+    //     $quiztest = Quiz::factory()->hasQcms(2)->create([
+    //         "name" => 'Quiz Test',
+    //         'is_public' => true,
+    //         'is_organization' => false,
+    //         'user_id' => $user->id
+    //     ]);
+
+
+    //     $quizData = [
+    //         'name' => 'Quiz updated',
+    //         'is_public' => false,
+    //         'is_organization' => true,
+    //         'qcms' => [
+    //             [
+    //                 'question' => 'Question updated',
+    //                 'answers' => [
+    //                     [
+    //                         'answer' => 'Answer 1 updated',
+    //                         'isValid' => true
+    //                     ],
+    //                     [
+    //                         'answer' => 'Answer 2 updated',
+    //                         'isValid' => false
+    //                     ],
+    //                     [
+    //                         'answer' => 'Answer 3 updated',
+    //                         'isValid' => false
+    //                     ],
+    //                     [
+    //                         'answer' => 'Answer 4 updated',
+    //                         'isValid' => true
+    //                     ]
+    //                 ]
+    //             ]
+    //         ]
+    //     ];
+
+    //     $response = $this->put('/api/quizzes/' . $quiztest->id, $quizData);
+    //     // $response->assertStatus(200);
+    //     $this->assertDatabaseHas('quizzes', ['name' => 'Quiz updated']);
+    // }
 }

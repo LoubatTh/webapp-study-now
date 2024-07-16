2b572800634d3d2430d789c02d243cec2cd625f2
Nathan Dulac <nathan.dulac@epitech.eu>
2024-07-16 09:51:56 +0200

    fix(organisationPage): Put a warning on the card if the organisation is empty

--- frontend/src/components/organizationsCard.tsx
+++ frontend/src/components/organizationsCard.tsx
@@ -4,6 +4,7 @@ import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Organization } from '@/types/organization.type';
 import { getColorClass } from '@/utils/tagscolor';
 import { useNavigate } from 'react-router-dom';
+import { CircleOff } from 'lucide-react';
 
 const OrganizationsCard = ( organization: Organization  ) => {
 
@@ -21,16 +22,27 @@ const OrganizationsCard = ( organization: Organization  ) => {
         <CardHeader>
           <CardTitle>{organization.name}</CardTitle>
           <CardDescription>{organization.description}</CardDescription>
-
         </CardHeader>
-        <CardContent className='flex flex-col gap-1'>
+        <CardContent className="flex flex-col gap-1">
+          {organization.tags.length == 0 && (
+            <>
+              <p className="text-amber-400 font-medium flex gap-3">
+                <CircleOff /> This organization is empty.
+              </p>
+            </>
+          )}
 
           {organization.tags.map((tag, index) => (
-            <p key={index} className={cn("p-1 ps-2 pe-2 rounded-lg font-medium text-sm", getColorClass(tag))}>
+            <p
+              key={index}
+              className={cn(
+                "p-1 ps-2 pe-2 rounded-lg font-medium text-sm",
+                getColorClass(tag)
+              )}
+            >
               {tag}
             </p>
           ))}
-          
         </CardContent>
         <CardFooter className="justify-between">
           <div className="flex items-center">

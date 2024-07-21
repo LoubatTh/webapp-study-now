97670d1762d96230657e7679d20051b6a7ef1e7e
Nathan Dulac <nathan.dulac@epitech.eu>
2024-07-21 18:40:58 +0200

    feat(ProfilePage): Add the account section tabs

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useUser } from "../contexts/UserContext";
-import { User, Mail, CheckCircle, XCircle, StarsIcon, StarIcon } from "lucide-react";
+import { User, Mail, CheckCircle, XCircle, StarsIcon, StarIcon, Phone } from "lucide-react";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -14,19 +14,22 @@ import { RegisterFormSchema } from "@/lib/form/register.form";
 import { fetchApi } from "@/utils/api";
 import { useAuth } from "@/contexts/AuthContext";
 import { motion } from "framer-motion";
+import { EditFormSchema } from "@/lib/form/edit.form";
+import { log } from "console";
+import { useToast } from "@/components/ui/use-toast";
 
 const ProfilePage = () => {
 
     const { id, name, email, is_subscribed } = useUser();
     const { accessToken } = useAuth();
+    const { toast } = useToast();
 
-    const registerForm = useForm({
-      resolver: zodResolver(RegisterFormSchema),
+    const editForm = useForm({
+      resolver: zodResolver(EditFormSchema),
       defaultValues: {
-        username: name || "",
-        email: email || "",
-        password: "",
-        confirmPassword: "",
+        username: "",
+        oldPassword: "",
+        newPassword: "",
       },
     });
 
@@ -34,44 +37,157 @@ const ProfilePage = () => {
 
       const body = {
         name: values.username,
-        email: values.email,
-        password: values.password,
+        password: values.oldPassword,
+        new_password: values.newPassword,
       };
-      
-      // TODO: A FINALISER (le back n'attends rien d'autres que le name)
-       const response = await fetchApi("PUT", "user", body, accessToken);
+
+      console.log(body)
+      const response = await fetchApi("PUT", "user", body, accessToken);
+      const status = await response.status;
+
+      if(status == 200){
+        toast({
+          title: "Profile updated",
+          description: "Your profile has been updated",
+          className: "bg-green-400",
+        })
+      } else {
+        const errorMessage = await response.error;
+        toast({
+          title: "Error",
+          description: errorMessage,
+          className: "bg-red-400",
+        });
+      }
 
     };
 
-    function handleResume() {
-      document.body.style.cursor = "wait";
-    }
 
-    function handleCancel() {
-      document.body.style.cursor = "wait";
-
-      try {
-        const response = fetchApi("POST", "stripe/cancel", null, accessToken);
-        console.log(response);
-      } catch (error) {
-        console.error("Error during the subscription process", error);
-      } finally {
-        document.body.style.cursor = "default";
-      
-      }
-
-    }
-
-  return (
-    <div className="">
-      <div className="bg-red-400 w-fit">
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+return (
+  <div className="flex justify-center w-full">
+    <div className="flex flex-row gap-4 w-3/4 max-w-5xl">
+      {/* Profile  */}
+      <div className="bg-white rounded-lg shadow-lg p-6 w-1/3">
+        <div className="flex justify-center">
+          <img
+            className="w-24 h-24 rounded-full"
+            src="https://picsum.photos/200/300"
+            alt="Profile"
+          />
+        </div>
+        <div className="text-center mt-4">
+          <h2 className="text-xl font-semibold">{name}</h2>
+          <p className="text-gray-600">Student</p>
+        </div>
+        <div className="flex flex-col gap-2 mt-4">
+          <div className="flex items-center mt-2">
+            <User className="text-gray-600" />
+            <span className="ml-2 text-gray-800">{name}</span>
+          </div>
+          <div className="flex items-center mt-2">
+            <Mail className="text-gray-600" />
+            <span className="ml-2 text-gray-800">{email}</span>
+          </div>
+          <div className="flex items-center mt-2">
+            <Phone className=" text-gray-600" />
+            <span className="ml-2 text-gray-800">+330000000</span>
+          </div>
+          <div className="flex items-center mt-2">
+            {is_subscribed ? (
+              <>
+                <StarsIcon className="text-green-500" />
+                <span className="ml-2 text-gray-800">Premium User</span>
+              </>
+            ) : (
+              <>
+                <XCircle className="text-red-500" />
+                <span className="ml-2 text-gray-800">Free User</span>
+              </>
+            )}
+          </div>
+        </div>
       </div>
+      <Tabs defaultValue="account" className="w-2/3">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="subscription">Subscription</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          <div className="bg-white rounded-lg p-8 shadow-lg">
+            <h1 className="text-xl font-semibold mb-2">Edit profile </h1>
+            <Form {...editForm}>
+              <form
+                onSubmit={editForm.handleSubmit(onChangeSaved)}
+                className="grid grid-cols-2 gap-4"
+              >
+                <FormField
+                  control={editForm.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem className="col-span-2">
+                      <FormLabel>New username</FormLabel>
+                      <FormControl>
+                        <Input placeholder={name} {...field} />
+                      </FormControl>
+                      <FormMessage className="min-h-6" />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={editForm.control}
+                  name="oldPassword"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Old Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="password"
+                          placeholder="********"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage className="min-h-6" />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={editForm.control}
+                  name="newPassword"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>New Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="password"
+                          placeholder="********"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage className="min-h-6" />
+                    </FormItem>
+                  )}
+                />
+                <div className="flex items-end col-span-2">
+                  <Button type="submit">Save Changes</Button>
+                </div>
+              </form>
+            </Form>
+          </div>
+        </TabsContent>
+        <TabsContent value="subscription">
+          <>
+            <div className="bg-white rounded-lg shadow-lg p-8">
+              <h1 className="text-xl font-semibold mb-2">
+                Manage subscription
+              </h1>
+              <p></p>
+            </div>
+          </>
+        </TabsContent>
+      </Tabs>
     </div>
-  );
+  </div>
+);
 };
 
 export default ProfilePage;

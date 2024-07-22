e2723501e6cfd6c4def1e10500b618ab7a8474c6
Nathan Dulac <nathan.dulac@epitech.eu>
2024-07-22 15:41:12 +0200

    fix(quizzDeckCard) Fix the avatar callback

--- frontend/src/components/quizzDeckCard.tsx
+++ frontend/src/components/quizzDeckCard.tsx
@@ -17,6 +17,7 @@ import LikeButton from "./LikeButton";
 import { Flashcard } from "@/types/deck.type";
 import { QCM } from "@/types/quizz.type";
 import { useUser } from "@/contexts/UserContext";
+import { User } from "lucide-react";
 
 type CommonCardProps = {
   id: number;
@@ -48,7 +49,7 @@ const QuizzDeckCard: React.FC<CommonCardProps> = ({
   onDeleteCard,
 }: CommonCardProps) => {
   const navigate = useNavigate();
-  const { name, avatar } = useUser();
+  const { name } = useUser();
   const [sizeCard, setSizeCard] = useState<number>(0);
   const cards = type === "Quiz" ? "quizz" : "deck";
   const itemLabel = type === "Quiz" ? "qcms" : "flashcards";
@@ -77,12 +78,10 @@ const QuizzDeckCard: React.FC<CommonCardProps> = ({
         <div className="flex justify-between">
           <CardHeader className="flex flex-row items-center p-3">
             <Avatar>
-              <AvatarImage
-                src={owner === name ? avatar : ""}
-                className="object-cover"
-                loading="lazy"
-              />
-              <AvatarFallback>{owner}</AvatarFallback>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>
+                <User />
+              </AvatarFallback>
             </Avatar>
             <div className="flex-col ml-2 capitalize text-wrap lg:text-nowrap lg:max-w-80 xl:max-w-60">
               <CardTitle className="md:truncate">{Cardname}</CardTitle>
